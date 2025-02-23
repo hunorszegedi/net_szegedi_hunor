ac503f3c71c938418dd975657c9e15ad22ae80a5
Hunor <szegedihunorofficial@gmail.com>
2025-02-23 15:43:45 +0200

    hazi feladat nagyjabol megoldva, szepitesre szorul

---
+++ Feladat_1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Feladat_1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Feladat_1/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Feladat_1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
