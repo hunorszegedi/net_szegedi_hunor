0383e9974534d2f4e1185cc7b2213feefbd6a584
Hunor <szegedihunorofficial@gmail.com>
2025-02-23 14:51:24 +0200

    Add project files.

---
+++ elso/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace elso
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ elso/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace elso
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
